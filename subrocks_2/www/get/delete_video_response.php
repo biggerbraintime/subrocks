489d67c65e65bfd04b0fca713c07f358de0d55f2
Matt <87334586+biggerbraintime@users.noreply.github.com>
2021-07-13 20:22:30 +0100

    put all initialized utils inside a file & removed initialize_db_var
    cry about it chief bazinga, i removed initialize_db_var

--- subrocks_2/www/get/delete_video_response.php
+++ subrocks_2/www/get/delete_video_response.php
@@ -1,19 +1,7 @@
 <?php ob_start(); ?>
 <?php require($_SERVER['DOCUMENT_ROOT'] . "/static/important/config.inc.php"); ?>
-<?php require($_SERVER['DOCUMENT_ROOT'] . "/static/lib/new/base.php"); ?>
-<?php require($_SERVER['DOCUMENT_ROOT'] . "/static/lib/new/fetch.php"); ?>
-<?php require($_SERVER['DOCUMENT_ROOT'] . "/static/lib/new/insert.php"); ?>
+<?php require($_SERVER['DOCUMENT_ROOT'] . "/static/important/initialized_utils.php"); ?>
 <?php
-    $_user_fetch_utils = new user_fetch_utils();
-    $_video_fetch_utils = new video_fetch_utils();
-    $_user_insert_utils = new user_insert_utils();
-    $_base_utils = new config_setup();
-    
-    $_base_utils->initialize_db_var($conn);
-    $_video_fetch_utils->initialize_db_var($conn);
-    $_user_fetch_utils->initialize_db_var($conn);
-    $_user_insert_utils->initialize_db_var($conn);
-
     $video_response = $_video_fetch_utils->get_video_response($_GET['id']);
     $video = $_video_fetch_utils->fetch_video_rid($video_response['toid']);
 ?>
